6120b24eaff1401d99dc112335c674871aa4ff25
Yashashwini0310 <Yashashwini222@gmail.com>
2025-03-23 23:29:26 +0000

    pushing custom admin page

--- job_portal/urls.py
+++ job_portal/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from jobs import views
 urlpatterns = [
     # path("", views.home, name="home"),
     path("admin/", admin.site.urls),
     path('', include("jobs.urls")),
     path('users/',include('users.urls')),
+    path('', RedirectView.as_view(url='/jobs/', permanent=False), name='root_redirect'),
 ]
